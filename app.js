bf218d3803f38d8e69321c84b90d785e00c7ca1f
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-09-04 10:38:32 +0800

    Add basic canvas and remove margins and padding

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const Application = PIXI.Application;
 
-const app = new Application();
+const app = new Application({
+    width: 500,
+    height: 500,
+    transparent: false,
+    antialias: true
+}); 
+
+// change the color of the canvas
+app.renderer.backgroundColor = 0x23395D; 
+
+// resize the canvas
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+// change the style of the canvas and set position to absolute
+// remove any default margins and paddings
+app.renderer.view.style.position = 'absolute';
+
+
+
+document.body.appendChild(app.view);
