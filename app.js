fe8e3c539e1eed76855be8f2b07dea3eb95b44ad
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-09-04 10:46:01 +0800

    Adding a simple rectangle to the stage

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 // remove any default margins and paddings
 app.renderer.view.style.position = 'absolute';
 
-
-
 document.body.appendChild(app.view);
+
+// shapes in pixi stored in the graphics class
+const Graphics = PIXI.Graphics;
+
+// every shape is an instance of it
+const rectangle = new Graphics();
+
+// transforming the shape into a rectangle
+rectangle.beginFill(0xAA33BB);
+rectangle.lineStyle(4, 0xFFEA00, 0.8); // linewidth, color, alpha (0-1)
+rectangle.drawRect(200, 200, 100, 120); // coords then width and height
+rectangle.endFill();
+
+// note the methods are chainable, can call them together for the same rectangle
+
+// call the stage and add the rectangle
+app.stage.addChild(rectangle);
+
+
